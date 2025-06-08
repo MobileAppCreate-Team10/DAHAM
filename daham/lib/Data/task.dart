49c878b8ee3d77daa50323f571824e3af6bd9eb1
JunYong An <happihanjy@naver.com>
2025-06-08 23:13:55 +0900

    feat: category, deadline 추가

--- daham/lib/Data/task.dart
+++ daham/lib/Data/task.dart
@@ -2,12 +2,16 @@ class Task {
   final String id;
   final String title;
   final String subject;
-  final Map<String, double> memberProgress; // {userId: 0.0~1.0}
+  final String category;
+  final DateTime? dueDate;
+  final Map<String, double> memberProgress;
 
   Task({
     required this.id,
     required this.title,
     required this.subject,
+    required this.category,
+    required this.dueDate,
     required this.memberProgress,
   });
 }
